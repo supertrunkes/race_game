90848b12dadbd286b2c14ede7e67003acefca1d5
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2017-08-23 16:27:02 -0700

    got css and html to where i want it

--- app.js
+++ app.js
@@ -1,31 +1,32 @@
 $(document).ready(function(){
   console.log('document ready')
-  let playerOneCount=0
-  let playerTwoCount=0
-  let winTarget = 10
+
+
   function beginRace(){
+    let playerOneCount=0
+    let playerTwoCount=0
+    let winTarget = 30
     $(document).on("keyup", function(e){
       if (playerOneCount<winTarget && playerTwoCount<winTarget){
 
-        // Player one
+        // Player one move
         if (e.keyCode === 90){
           playerOneCount++;
           console.log(playerOneCount);
-          $(".player_1").animate({left: "+=10%"},0);
-          if (playerOneCount === winTarget){
-            alert("Player One wins!");
-          }}
-          // Player two
+          $(".player_1").animate({left: "+=2.5%"},100);
+          }
+          // Player two move
           else if (e.keyCode === 77) {
             playerTwoCount++
             console.log(playerTwoCount);
-            $(".player_2").animate({left: "+=10%"},0);
-            if (playerTwoCount === winTarget){
-              alert("Player Two wins!");
-            }
+            $(".player_2").animate({left: "+=2.5%"},100);
           }
         } else {console.log("game over");
-        $(document).off("keyup");
+        $(document).off("keyup");if (playerOneCount === winTarget){
+          alert("Batman wins!");
+        }else if (playerTwoCount === winTarget){
+          alert("Pikachu wins!");
+        }
       }
     });
   }

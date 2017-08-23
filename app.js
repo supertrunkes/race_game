88016705709fd352155d267e26f81bdbb5a40519
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2017-08-23 14:37:59 -0700

    got players moveing messed with css a lot

--- app.js
+++ app.js
@@ -2,21 +2,33 @@ $(document).ready(function(){
   console.log('document ready')
   let playerOneCount=0
   let playerTwoCount=0
-  let winTarget = 40
-  $(document).keyup( function(e){
-		if (e.keyCode === 90){
-      playerOneCount++
-      console.log(playerOneCount);
-      if (playerTwoCount === winTarget){
-        alert("Player One wins!");
+  let winTarget = 10
+  function beginRace(){
+    $(document).on("keyup", function(e){
+      if (playerOneCount<winTarget && playerTwoCount<winTarget){
+
+        // Player one
+        if (e.keyCode === 90){
+          playerOneCount++;
+          console.log(playerOneCount);
+          $(".player_1").animate({left: "+=10%"},0);
+          if (playerOneCount === winTarget){
+            alert("Player One wins!");
+          }}
+          // Player two
+          else if (e.keyCode === 77) {
+            playerTwoCount++
+            console.log(playerTwoCount);
+            $(".player_2").animate({left: "+=10%"},0);
+            if (playerTwoCount === winTarget){
+              alert("Player Two wins!");
+            }
+          }
+        } else {console.log("game over");
+        $(document).off("keyup");
       }
-    } else if (e.keyCode === 77) {
-      playerTwoCount++
-      console.log(playerTwoCount);
-      if (playerTwoCount === winTarget){
-        alert("Player Two wins!");
-      }
-    }
-})
-})
+    });
+  }
+  beginRace();
+});
 console.log("js ready");

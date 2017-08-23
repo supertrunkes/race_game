c5d30315add0c80cf0e5aff2d91396f757327c66
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2017-08-23 10:26:30 -0700

    basic game functionality

--- app.js
+++ app.js
@@ -1,4 +1,22 @@
 $(document).ready(function(){
   console.log('document ready')
+  let playerOneCount=0
+  let playerTwoCount=0
+  let winTarget = 40
+  $(document).keyup( function(e){
+		if (e.keyCode === 90){
+      playerOneCount++
+      console.log(playerOneCount);
+      if (playerTwoCount === winTarget){
+        alert("Player One wins!");
+      }
+    } else if (e.keyCode === 77) {
+      playerTwoCount++
+      console.log(playerTwoCount);
+      if (playerTwoCount === winTarget){
+        alert("Player Two wins!");
+      }
+    }
+})
 })
 console.log("js ready");

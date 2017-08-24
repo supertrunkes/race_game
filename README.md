a1de55deecde27f6d05b724e4549653cedf52810
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2017-08-23 18:57:40 -0700

    added modal for the end of the game

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # racing_game
-A game where you race.
+Ever wonder what would happen if Batman and Pikachu got in a foot race?
+
+Well this state of the art simulator will let you and a friend find out.
--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
   console.log('document ready')
 
-
   function beginRace(){
     let playerOneCount=0
     let playerTwoCount=0
@@ -12,21 +11,26 @@ $(document).ready(function(){
         // Player one move
         if (e.keyCode === 90){
           playerOneCount++;
-          console.log(playerOneCount);
           $(".player_1").animate({left: "+=2.5%"},100);
           }
           // Player two move
           else if (e.keyCode === 77) {
             playerTwoCount++
-            console.log(playerTwoCount);
             $(".player_2").animate({left: "+=2.5%"},100);
           }
+          //end of game protocal
         } else {console.log("game over");
         $(document).off("keyup");if (playerOneCount === winTarget){
-          alert("Batman wins!");
+          $(".modal-body").append("<h2> Batman has won the race!<h2<");
+          $('#myModal').modal('show');
         }else if (playerTwoCount === winTarget){
-          alert("Pikachu wins!");
+          $(".modal-body").append("<h2> Pikachu has won the race!<h2<");
+          $('#myModal').modal('show');
         }
+        //if modal is closed without starting new game a play again button is added
+        $('#myModal').on('hidden.bs.modal', function (e) {
+        $("body").append('<button class="btn btn-danger"  onClick="window.location.reload()">Play agian?</button>')
+        })
       }
     });
   }
